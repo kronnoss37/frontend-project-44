d41980061f48c03b9475e7d3ce15de80995ccb4d
kronnoss <kronnoss.it@gmail.com>
2025-04-11 19:06:22 +0300

    Fix Eslint errors in src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default function greetingUser(){
-	const userName = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${userName}!`);
-	return userName;
+export default function greetingUser() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 }
