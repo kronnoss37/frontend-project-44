23de086878804f47050eab5b4d19f9261bb8b8c8
kronnoss <kronnoss.it@gmail.com>
2025-04-11 18:46:45 +0300

    Initialize ESlint with eslint.config.mjs and dev dependencies

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: { import: importPlugin },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+      'import/extensions': 'off',
+      'no-console': 'off',
+    },
+  },
+];
