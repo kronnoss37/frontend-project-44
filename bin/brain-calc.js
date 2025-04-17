24b6a74ab1e84f896b2acaa9f1bdc5744ea94d57
kronnoss <kronnoss.it@gmail.com>
2025-04-17 17:59:00 +0300

    Add file bin/brain-calc.js with the necessary logic

---
+++ bin/brain-calc.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { getRandomExpression, communicationWithUser } from '../src/index.js';
+
+const getCorrectAnswerInThisGame = (mathExpression) => {
+  const arrayElements = mathExpression.split(' ');
+  const mathOperator = arrayElements[1];
+  const firstOperand = Number(arrayElements[0]);
+  const secondOperand = Number(arrayElements[2]);
+
+  switch (mathOperator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      return null;
+  }
+};
+
+const rulesOfTheGame = 'What is the result of the expression?';
+
+communicationWithUser(rulesOfTheGame, getRandomExpression, getCorrectAnswerInThisGame);
