e4d0fac17813c3c246b8bd333108b02171e92025
kronnoss <kronnoss.it@gmail.com>
2025-04-17 18:03:04 +0300

    Change the structure in bin/brain-calc.js because of the addition of src/index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,38 +1,9 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import greetingUser from '../src/cli.js';
+import { getRandomNumber, communicationWithUser } from '../src/index.js';
 
-const getRandomNumber = () => Math.floor(Math.random() * 101);
+const getCorrectAnswerInThisGame = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
-const getCorrectAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const rulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const normalizeUserAnswer = (userAnswer) => userAnswer.trim().toLowerCase();
-
-const communicationWithUser = () => {
-  const userName = greetingUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const amountOfCorrectAnswers = 3;
-  let countCorrectAnswers = 0;
-
-  while (countCorrectAnswers < amountOfCorrectAnswers) {
-    const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    let userAnswer = readlineSync.question('Your answer: ');
-    userAnswer = normalizeUserAnswer(userAnswer);
-    const correctAnswer = getCorrectAnswer(randomNumber);
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-      countCorrectAnswers += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-      return false;
-    }
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-  return true;
-};
-
-communicationWithUser();
+communicationWithUser(rulesOfTheGame, getRandomNumber, getCorrectAnswerInThisGame);
