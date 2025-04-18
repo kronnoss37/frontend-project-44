e84081a7292505a26ee260efd61b25230246269c
kronnoss <kronnoss.it@gmail.com>
2025-04-18 23:07:05 +0300

    Update functionality in bin/games/brain-even.js

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-import { getRandomNumber, communicationWithUser } from '../../src/index.js';
+import {
+  getRandomNumber, communicationWithUser, makeQuestion, isEvenNumber,
+} from '../../src/index.js';
 
-const getCorrectAnswerInThisGame = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const getCorrectAnswerInThisGame = () => {
+  const num = getRandomNumber();
+  makeQuestion(num);
+  return isEvenNumber(num);
+};
 
 const rulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-communicationWithUser(rulesOfTheGame, getRandomNumber, getCorrectAnswerInThisGame);
+communicationWithUser(rulesOfTheGame, getCorrectAnswerInThisGame);
