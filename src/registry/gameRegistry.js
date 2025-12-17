d3c9446312af4cc0a6bd792d9b46b6670914a055
kronnoss <kronnoss.it@gmail.com>
2025-12-17 21:17:20 +0300

    Add registry file for easy access to games

---
+++ src/registry/gameRegistry.js
@@ -0,0 +1,17 @@
+import runDefineEvenGame from '../games/even.js'
+import runCalculatorGame from '../games/calculator.js'
+import runDefineGCDGame from '../games/gcd.js'
+import runProgressionGame from '../games/progression.js'
+import runDefinePrimeGame from '../games/prime.js'
+
+const gameList = {
+  1: runDefineEvenGame,
+  2: runCalculatorGame,
+  3: runDefineGCDGame,
+  4: runProgressionGame,
+  5: runDefinePrimeGame,
+}
+
+const defineGame = gameNumber => gameList[gameNumber]
+
+export default defineGame
